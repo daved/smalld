43dbb3282c07ad830f1b64bbdd47c4a8bd049b9a
Daved <daved@codemodus.com>
2016-02-10 18:49:14 -0800

    Continue refactoring node.

--- node.go
+++ node.go
@@ -46,18 +46,6 @@ func (n *node) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	n.mux.ServeHTTP(w, r)
 }
 
-func (n *node) recordlocations(v *url.Values) {
-	p := makePoint(v)
-	lbl := fmt.Sprintf("%s", v.Get("label"))
-
-	acc, err := strconv.ParseFloat(v.Get("acc"), 64)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	n.db.AddLocations(lbl, acc, p)
-}
-
 // LocationHandler is the main entry point for smalld
 // it receives the get request parses the location data from it
 // and logs the values to the location table.
@@ -69,44 +57,35 @@ func (n *node) LocationHandler(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
+	txt422 := "bad or missing parameters"
 	if req.URL.RawQuery == "" {
-		http.Error(w, "bad or missing parameters", 422)
+		http.Error(w, txt422, 422)
 		return
 	}
 
 	vals, err := url.ParseQuery(req.URL.RawQuery)
 	if err != nil {
-		// TODO: respond
+		http.Error(w, txt422, 422)
 		return
 	}
 
-	log.Println(vals)
-
-	if !safeValues(&vals) {
-		// TODO: respond
+	l, a, p, err := labelAccPoint(vals)
+	if err != nil {
+		http.Error(w, txt422, 422)
 		return
 	}
 
-	p := makePoint(&vals)
-	log.Println("point:", p)
+	fmt.Println(l, a, p)
+	go n.db.AddLocations(l, a, p)
 
-	lbl := fmt.Sprintf("%s", vals.Get("label"))
-
-	acc, err := strconv.ParseFloat(vals.Get("acc"), 64)
+	ls, err := n.db.LocationsNameByPoint(p)
 	if err != nil {
-		log.Fatal(err)
-	}
-
-	go n.db.AddLocations(lbl, acc, p)
-
-	l, err := n.db.LocationsNameByPoint(p)
-	if err != nil {
-		// TODO: respond
+		http.Error(w, http.StatusText(500), 500)
 		return
 	}
 
 	m := make(map[string][]string)
-	m["names"] = l
+	m["names"] = ls
 
 	j, err := json.Marshal(m)
 	if err != nil {
@@ -117,19 +96,17 @@ func (n *node) LocationHandler(w http.ResponseWriter, req *http.Request) {
 	h.Add("Access-Control-Allow-Origin", "*")
 
 	w.Write(j)
-
-	return
-
 }
 
-func safeValues(v *url.Values) bool {
-	log.Printf("safe %+v", v)
+func labelAccPoint(v url.Values) (string, float64, string, error) {
+	l := fmt.Sprintf("%s", v.Get("label"))
 
-	return true //for now
-}
+	a, err := strconv.ParseFloat(v.Get("acc"), 64)
+	if err != nil {
+		return "", 0, "", err
+	}
 
-func makePoint(v *url.Values) string {
 	p := fmt.Sprintf("POINT(%s %s)", v.Get("lon"), v.Get("lat"))
 
-	return p
+	return l, a, p, nil
 }
